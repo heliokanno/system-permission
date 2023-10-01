38c6ed2ae1d88955f0e1300717aa6cdbc362889f
helio <heliok.dev@gmail.com>
2023-10-01 21:54:03 +0100

    adding config flyway and migrations

--- src/test/java/com/hmk/system/permission/SystemPermissionApplicationTests.java
+++ src/test/java/com/hmk/system/permission/SystemPermissionApplicationTests.java
@@ -2,12 +2,14 @@ package com.hmk.system.permission;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations="classpath:application-test.properties")
 class SystemPermissionApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
