3c8c4e0eaae0ad900d3eb7a2b21b50d10e1385ab
helio <heliok.dev@gmail.com>
2023-10-01 20:55:39 +0100

    first commit

---
+++ src/main/java/com/hmk/system/permission/SystemPermissionApplication.java
@@ -0,0 +1,13 @@
+package com.hmk.system.permission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemPermissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemPermissionApplication.class, args);
+    }
+
+}
